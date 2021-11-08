37bb3b51927b8fd653a21eaa64a77d204c86f44a
Mariia Grinko <mariiapryashnikova@gmail.com>
2021-11-08 14:59:03 +0300

    done

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,8 +3,10 @@ let menuLink = document.querySelectorAll(".menu__link")
 
 for (let i = 0; i < menuLink.length; i++) {
     menuLink[i].onclick = function() {
-        this.closest(".menu__item").querySelector(".menu_sub").classList.toggle("menu_active");
-        return false;
+        let subMenu = this.closest(".menu__item").querySelector(".menu_sub");
+        if (subMenu != null) {
+            subMenu.classList.toggle("menu_active");
+            return false;
+        }
     }
-
 };
